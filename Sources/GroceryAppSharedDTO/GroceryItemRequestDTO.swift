db1c01577d892d6421d3863750f5b966e889970d
Raphael Torquato <raphael.martini@live.com>
2023-05-26 23:01:15 -0300

    updating the GroceryItemRequestDTO
    updating the GroceryItemRequestDTO

--- Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -9,13 +9,11 @@ import Foundation
 
 public struct GroceryItemRequestDTO: Codable {
     
-    public let id: UUID
     public let title: String
     public let price: Double
     public let quantity: Int
     
-    public init(id: UUID, title: String, price: Double, quantity: Int) {
-        self.id = id
+    public init(title: String, price: Double, quantity: Int) {
         self.title = title
         self.price = price
         self.quantity = quantity
